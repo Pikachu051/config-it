24819f57c60bd74defc739e08998d0e0f265c04a
cheraxm <ctk.jk.ca14@gmail.com>
2024-04-01 14:57:51 +0700

    update

--- main.py
+++ main.py
@@ -70,14 +70,14 @@ async def show_run_int(ctx):
         output = net_connect.send_command('show run int')
         await ctx.send(output)
 
-async def coopy_run_conf(ctx):
+async def copy_run_conf(ctx):
     if net_connect == None:
         await ctx.send('You need to connect to a device first!\n\nUse !connect <ip> <username> <password> to connect to a device.')
     else:
-        output = net_connect.send_command('coppy run conf')
+        output = net_connect.send_command('copy run conf')
         await ctx.send(output)
 
-async def coopy_run_conf(ctx, hostname):
+async def hostname(ctx, hostname):
     if net_connect == None:
         await ctx.send('You need to connect to a device first!\n\nUse !connect <ip> <username> <password> to connect to a device.')
     else:
