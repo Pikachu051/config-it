a21ee2c3516d52f9e3b691c76e0c8a15e03dafa3
Sahachindech Katedee <86822195+Pikachu051@users.noreply.github.com>
2024-04-01 12:40:55 +0700

    update command

--- main.py
+++ main.py
@@ -3,14 +3,30 @@ import os
 from dotenv import load_dotenv
 from discord.ext import commands
 import discord
-from resources import get_response
+from netmiko import ConnectHandler
 
 load_dotenv()
 TOKEN: Final[str] = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID: Final[int] = int(os.getenv("CHANNEL_ID"))
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print('Bot is ready!')
+    channel = bot.get_channel(CHANNEL_ID)
+    await channel.send('Bot is ready!')
+
+@bot.command()
+async def connect(ctx, ip, username, password):
+    device = {
+        'device_type': 'cisco_ios',
+        'host': ip,
+        'username': username,
+        'password': password,
+        'port': 22,
+    }
+    await ctx.send(f'Connecting to {ip}...')
+
+bot.run(TOKEN)
 
--- resources.py
+++ resources.py
@@ -1,10 +1,10 @@
 from netmiko import ConnectHandler
 
 
-def get_response(user_input: str) -> str:
-    lowered: str = user_input.lower()
+# def get_response(user_input: str) -> str:
+#     lowered: str = user_input.lower()
 
-    if lowered == '':
-        return "You didn't say anything!"
-    elif lowered == '!connect':
+#     if lowered == '':
+#         return "You didn't say anything!"
+#     elif lowered == '!connect':
 
