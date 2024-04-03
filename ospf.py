4d72dcb4bf860bdaa4fd37019278c0311b7459f0
cheraxm <ctk.jk.ca14@gmail.com>
2024-04-03 16:08:01 +0700

    Update ospf.py

--- ospf.py
+++ ospf.py
@@ -1,8 +1,14 @@
-def router_ospf(network):
+from discord.ext import commands
+import discord
+from netmiko import ConnectHandler
+from net_cal import wildcard_mask
+
+def router_ospf(networks):
         networks = networks.split(',')
         networks = [network.split('/') for network in networks]
         commands = []
         commands.append('router ospf 1')
         for network in networks:
+            network[1] = wildcard_mask(int(network[1]))
             commands.append(f'network {network[0]} {network[1]} area {network[2]}')
         return commands
