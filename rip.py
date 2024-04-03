adc5fe7cc1f094deb9f842d55884f355dc78b1a7
cheraxm <ctk.jk.ca14@gmail.com>
2024-04-03 17:31:15 +0700

    Update rip.py

--- rip.py
+++ rip.py
@@ -8,3 +8,13 @@ def rip(network):
     for network in networks:
         configs.append('network ' + network)
     return configs
+
+def remove_rip_nw(network):
+    configs = ['router rip']
+    networks = network.split(',')
+    for network in networks:
+        configs.append('no network ' + network)
+    return configs
+
+def disable_rip():
+    return ['no router rip']
