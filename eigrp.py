d3c0d7ead193eaf45d0e4c962f4bc0eb0abba69b
cheraxm <ctk.jk.ca14@gmail.com>
2024-04-03 17:31:05 +0700

    Update eigrp.py

--- eigrp.py
+++ eigrp.py
@@ -4,7 +4,7 @@ from netmiko import ConnectHandler
 from net_cal import wildcard_mask
 # import asyncio
 
-async def route_eigrp(networks, asn): 
+async def eigrp(networks, asn): 
     networks = networks.split(',')
     networks = [network.split('/') for network in networks]
     commands = []
@@ -13,6 +13,19 @@ async def route_eigrp(networks, asn):
         network[1] = wildcard_mask(int(network[1]))
         commands.append (f'network {network[0]} {network[1]}')
     return commands
+
+def remove_eigrp_nw(networks, asn):
+    networks = networks.split(',')
+    networks = [network.split('/') for network in networks]
+    commands = []
+    commands.append('router eigrp ' + asn)
+    for network in networks:
+        network[1] = wildcard_mask(int(network[1]))
+        commands.append(f'no network {network[0]} {network[1]}')
+    return commands
+
+def disable_eigrp(asn):
+    return ['no router eigrp '+ asn]
 
 
 
