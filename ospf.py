77b3c84c8895854e356333b72c14217f8148bf2a
cheraxm <ctk.jk.ca14@gmail.com>
2024-04-03 17:31:11 +0700

    Update ospf.py

--- ospf.py
+++ ospf.py
@@ -12,3 +12,16 @@ def ospf(networks):
             network[1] = wildcard_mask(int(network[1]))
             commands.append(f'network {network[0]} {network[1]} area {network[2]}')
         return commands
+ 
+def remove_ospf_nw(networks):
+    networks = networks.split(',')
+    networks = [network.split('/') for network in networks]
+    commands = []
+    commands.append('router ospf 1')
+    for network in networks:
+        network[1] = wildcard_mask(int(network[1]))
+        commands.append(f'no network {network[0]} {network[1]} area {network[2]}')
+    return commands
+
+def disable_ospf():
+    return ['no router ospf 1']
